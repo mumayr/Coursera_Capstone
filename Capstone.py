aba6adb7e23fe36d2b580c43778f857fcc739817
mumayr <36114339+mumayr@users.noreply.github.com>
2021-01-03 21:35:17 -0600

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science IBM Professional Certificate Capstone
+# 
+# ## This File would include the following
+# 
+# #### Some markdown to explain that this notebook will be mainly used for the capstone project.
+# #### Import the pandas library as pd.
+# #### Import the Numpy library as np.
+# #### Print the following the statement: Hello Capstone Project Course!
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
